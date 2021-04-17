14b972bbc0e3bca8b53e750c71256aa1643322d4
Mahmood Ahmad <mahmood073@gmail.com>
2021-04-17 13:05:04 +0200

    handeling API response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,14 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use App\Traits\Response\SendResponse;
 
 class Handler extends ExceptionHandler
 {
+    use SendResponse;
+    
     /**
      * A list of the exception types that are not reported.
      *
@@ -36,6 +41,28 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (AuthenticationException $e, $request) {
+            return $this->sendResponse([], $e->getMessage(), false, 401);
+        });
+
+        $this->renderable(function (ValidationException $e, $request) {
+            $errors = [];
+
+            foreach ($e->errors() as $field => $error) {
+                $errors[] = [
+                    'field'   => $field,
+                    'message' => $error[0]['message'],
+                    'code'    => (int)$error[0]['code'],
+                ];
+            }
+    
+            return $this->sendResponse($errors, $e->getMessage(), false, 422);
+        });
+
+        $this->renderable(function (Throwable $e, $request) {
+            return $this->sendExceptionResponse($e, false);
         });
     }
 }
